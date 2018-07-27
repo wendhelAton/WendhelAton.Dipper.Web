f3df51e495ae1ca7f5dbb4d416c9012dd1538531
wendhelAton <wendhelgregorio@gmail.com>
2018-07-27 18:56:18 +0800

    Setting up

---
+++ WendhelAton.Dipper.Web/WendhelAton.Dipper.Web/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace WendhelAton.Dipper.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options => { options.Listen(IPAddress.Loopback, 6001); })
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
